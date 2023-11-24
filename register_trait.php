94aac17e22d95ee525d5a9d46d695220efc9112c
Mouhcine Essafi <messafi1337@gmail.com>
2023-11-24 22:53:32 +0100

    Create register_trait.php

---
+++ register_trait.php
@@ -0,0 +1,30 @@
+<?php
+
+   include 'connection.php';
+
+if(isset($_POST['submit'])){
+
+   $name = $_POST['fullname'];
+   $email = $_POST['email'];
+   $phone = $_POST['phone'];
+   $pass = $_POST['password']; 
+   $user_type = $_POST['role'];
+
+   $select = " SELECT * FROM $table_users WHERE email = '$email' ";
+
+   $result = mysqli_query($conn, $select);
+
+   if(mysqli_num_rows($result) > 0){
+
+      $error[] = 'user already exist!';
+
+   }else{
+
+      $insert = "INSERT INTO $table_users(fullname, phone, role, email, password) VALUES('$name','$phone','$user_type','$email','$pass')";
+         mysqli_query($conn, $insert);
+         header('location:register_form.php');
+   }
+
+};
+$conn->close();
+?>
