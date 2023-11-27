28e35e88e9e16b0c9e128f2ce9b4190b24dcc163
Mouhcine Essafi <messafi1337@gmail.com>
2023-11-27 21:24:30 +0100

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,11 +1,51 @@
 <?php
 session_start();
 ?>
-<div class="bg-blue-800 p-4 w-full animate-pulse flex justify-end z-10">
-    <a href="nnonces.php" class="text-white"><?php echo "Accueil"  ?></a>
+<!-- <div class="bg-blue-800 p-4 w-full animate-pulse flex justify-end z-10">
+    <a href="Annonces.php" class="text-white"><?php echo "Accueil"  ?></a>
     <a href="list_user.php" class="text-white"><?php if($_SESSION['user_type'] == 'admin'){echo "all users";}  ?></a>
     <a href="mes_annonces.php" class="text-white"><?php if($_SESSION['user_type'] == 'Annoncer'){echo "Mes annonces";}  ?></a>
     <a href="add_annonce.php" class="text-white"><?php if($_SESSION['user_type'] == 'Annoncer' || $_SESSION['user_type'] == 'admin'){echo "Ajouter une annonce";} ?></a>
     <a href="edit_profile.php" class="text-white"><?php if($_SESSION['user_type'] == 'admin'){ echo "edit profile";} ?></a>
     <a href="logout.php" class="text-white"><?php echo "Se déconnecter"; ?></a>
 </div>
+ -->
+
+
+
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+      <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Avito</span>
+  </a>
+  <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+      <a  href="logout.php" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Se déconnecter</a>
+      <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+  </div>
+  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <li>
+        <a href="Annoncer.php" class="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500" aria-current="page">Accueil</a>
+      </li>
+      <li>
+        <a href="list_user.php" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"><?php if($_SESSION['user_type'] == 'admin'){echo "all users";}  ?></a>
+      </li>
+      <li>
+        <a href="mes_annonces.php" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"><?php if($_SESSION['user_type'] == 'Annoncer'){echo "Mes annonces";}  ?></a>
+      </li>
+      <li>
+        <a href="add_annonce.php" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"><?php if($_SESSION['user_type'] == 'Annoncer' || $_SESSION['user_type'] == 'admin'){echo "Ajouter une annonce";} ?></a>
+      </li>
+      <li>
+        <a href="edit_profile.php" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">edit profile</a>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
