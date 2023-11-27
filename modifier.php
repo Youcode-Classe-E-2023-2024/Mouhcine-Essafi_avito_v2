287bd9f3d22c08cb95cf25e93aa467ea1eecd661
Mouhcine Essafi <messafi1337@gmail.com>
2023-11-27 16:14:12 +0100

    Update modifier.php

--- modifier.php
+++ modifier.php
@@ -43,8 +43,8 @@ $row = mysqli_fetch_assoc($result);
         <div class="col-span-full">
           <label for="about" class="block text-sm font-medium leading-6 text-gray-900">About</label>
           <div class="mt-2">
-            <textarea value="<?php echo $row['about'] ?>" id="about" name="about" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
-          </div>
+            <textarea id="about" name="about" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"><?php echo $row['about'] ?></textarea>
+            </div>
           <p class="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about your annonce.</p>
         </div>
 
