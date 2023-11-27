ebd1cba35d8679109c2f7110fb5e87ae8e6e46b2
Mouhcine Essafi <messafi1337@gmail.com>
2023-11-27 09:19:47 +0100

    Update update.php

--- update.php
+++ update.php
@@ -1,14 +1,12 @@
 <?php
-include_once ("creation.php");
+include_once ("connection.php");
 
     $id = $_POST["id"];
-    $titre = $_POST["titre"];
-    $description = $_POST["description"];
-    $prix = $_POST["prix"];
-    $telephone = $_POST["telephone"];
-    $email = $_POST["email"];
+    $titre = $_POST["title"];
+    $description = $_POST["about"];
+    $prix = $_POST["price"];
 
-    $sql = "UPDATE $table_annonces SET title='$titre', about='$description', price=$prix' WHERE id=$id";
+    $sql = "UPDATE $table_annonces SET title='$titre', about='$description', price='$prix' WHERE id = $id";
 
     $res = mysqli_query($conn, $sql);
 
