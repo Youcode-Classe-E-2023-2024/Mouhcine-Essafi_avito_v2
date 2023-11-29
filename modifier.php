2d27bef2937fb8dc92b9c03602012aa7425d957c
Mouhcine Essafi <messafi1337@gmail.com>
2023-11-29 03:24:41 +0100

    Update modifier.php

--- modifier.php
+++ modifier.php
@@ -11,80 +11,58 @@ $row = mysqli_fetch_assoc($result);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <title>Avito.ma</title>
 </head>
-<body>
+<body class="bg-gray-100 flex items-center justify-center h-screen">
 
-        <style>
-          /* Hide the number input arrows */
-          input[type="number"]::-webkit-inner-spin-button,
-          input[type="number"]::-webkit-outer-spin-button {
-              -webkit-appearance: none;
-          }
-        </style>
+    <style>
+        /* Hide the number input arrows */
+        input[type="number"]::-webkit-inner-spin-button,
+        input[type="number"]::-webkit-outer-spin-button {
+            -webkit-appearance: none;
+        }
+    </style>
 
-<form class="ml-64 mr-64 mt-12 mb-12" method="POST" action="update.php" enctype="multipart/form-data">
-<input type="hidden" name="id" value="<?php echo $id ?>">
-  <div class="space-y-12">
-    <div class="border-b border-gray-900/10 pb-12">
-      <h2 class="text-2xl font-semibold leading-7 text-gray-900 mt-8">Announcement Information</h2>
+    <form class="w-full max-w-lg bg-white p-8 rounded-lg shadow-lg" method="POST" action="update.php" enctype="multipart/form-data">
+        <input type="hidden" name="id" value="<?php echo $id ?>">
+        <h2 class="text-3xl font-semibold text-gray-900 mb-8">Announcement Information</h2>
 
-      <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-        <div class="sm:col-span-3">
-          <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
-          <div class="mt-2">
-            <input type="text" value="<?php echo $row['title'] ?>" name="title" id="title" autocomplete="off" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
-
-        
-
-        <div class="col-span-full">
-          <label for="about" class="block text-sm font-medium leading-6 text-gray-900">About</label>
-          <div class="mt-2">
-            <textarea id="about" name="about" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"><?php echo $row['about'] ?></textarea>
+        <div class="grid grid-cols-1 gap-6">
+            <div>
+                <label for="title" class="block text-sm font-medium text-gray-700">Title</label>
+                <input type="text" name="title" id="title" value="<?php echo $row['title'] ?>" autocomplete="off" class="mt-1 p-2 w-full border rounded-md text-gray-700 focus:outline-none focus:ring focus:border-indigo-500">
             </div>
-          <p class="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about your annonce.</p>
-        </div>
 
-
-        <div class="sm:col-span-1">
-          <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Price</label>
-          <div class="mt-2">
-            <input type="number" value="<?php echo $row['price'] ?>" name="price" id="last-name" autocomplete="off" class="butt block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-          </div>
-        </div>
-
-
-        <div class="col-span-full">
-          <label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900">Annonce photo</label>
-          <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-            <div class="text-center">
-              <svg class="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
-              </svg>
-              <div class="mt-4 flex text-sm leading-6 text-gray-600">
-                <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
-                  <span>Upload a file</span>
-                  <input id="file-upload" value="<?php echo $row['img'] ?>" name="img" type="file" class="sr-only">
-                </label>
-                <p class="pl-1">or drag and drop</p>
-              </div>
-              <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+            <div>
+                <label for="about" class="block text-sm font-medium text-gray-700">About</label>
+                <textarea id="about" name="about" rows="3" class="mt-1 p-2 w-full border rounded-md text-gray-700 focus:outline-none focus:ring focus:border-indigo-500"><?php echo $row['about'] ?></textarea>
+                <p class="mt-2 text-sm text-gray-500">Write a few sentences about your announcement.</p>
             </div>
-          </div>
+
+            <div>
+                <label for="price" class="block text-sm font-medium text-gray-700">Price</label>
+                <input type="number" name="price" id="price" value="<?php echo $row['price'] ?>" autocomplete="off" class="mt-1 p-2 w-full border rounded-md text-gray-700 focus:outline-none focus:ring focus:border-indigo-500">
+            </div>
+
+            <div>
+                <label for="img" class="block text-sm font-medium text-gray-700">Announcement Photo</label>
+                <div class="mt-1 flex items-center">
+                    <label for="file-upload" class="relative cursor-pointer bg-white border border-gray-300 p-2 rounded-md font-semibold text-indigo-600 hover:text-indigo-500">
+                        <span>Upload a file</span>
+                        <input id="file-upload" name="img" type="file" value="<?php echo $row['img'] ?>" class="sr-only">
+                    </label>
+                    <p class="pl-3">or drag and drop</p>
+                </div>
+                <p class="mt-2 text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+            </div>
         </div>
-      </div>
-    </div>
 
-    
-  </div>
+        <div class="mt-8 flex items-center justify-end gap-x-4">
+            <a href="mes_annonces.php" class="text-sm text-gray-700 hover:text-indigo-500">Cancel</a>
+            <button type="submit" name="submit" class="px-4 py-2 bg-indigo-600 text-sm font-semibold text-white rounded-md shadow-md hover:bg-indigo-500 focus:outline-none focus:ring focus:border-indigo-500">Save</button>
+        </div>
+    </form>
 
-  <div class="mt-6 flex items-center justify-end gap-x-6">
-    <a href="mes_annonces.php"><button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button></a>
-    <button type="submit" name="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
-  </div>
-</form>
 </body>
 </html>
